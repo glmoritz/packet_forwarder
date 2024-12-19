8fa9b076d62d2466615c3c010eb85845d1b773ca
marcoseduardopm <marcose@utfpr.edu.br>
2024-12-19 15:48:38 -0300

    Update fix_symlinks.sh setting LabSCim as the main folder.

--- lora_pkt_fwd/inc/fix_symlinks.sh
+++ lora_pkt_fwd/inc/fix_symlinks.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 rm lr_fhss_v1_base_types.h 
-ln -s $HOME/LoRaMac-node/src/radio/labscim_sx126x_driver/src/lr_fhss_v1_base_types.h
+ln -s $HOME/LabSCim/LoRaMac-node/src/radio/labscim_sx126x_driver/src/lr_fhss_v1_base_types.h
